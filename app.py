1c2c86f58de5fcbddffd910c720ef82b34daa521
Sebastian Rodriguez <seba_sur90@hotmail.com>
2022-04-24 09:38:08 -0300

    Actualizo app

--- app.py
+++ app.py
@@ -10,13 +10,13 @@ import pandas as pd
 from funciones_para_datasets import procesa_cotizacion
 from funciones_para_datasets import procesar_datos
 from paginas.pagina_ingresos import pagina_ingresos_funcion
-
+from paginas.pagina_movimientos import pagina_movimientos_funcion
 
 def main():
     pages = {
         "Home": page_home,
         "pagina_ingresos":pagina_ingresos_funcion,
-        # "Preprocesamiento": prepro,
+        "Movimientos": pagina_movimientos_funcion,
         # "PCA":page_pca,
         # "Random forest":random_forest,
         # "KNN: vecinos mas cercanos":page_knn
@@ -45,35 +45,20 @@ def page_home():
     st.title("Dashboard Credicoop")
     uploaded_file = st.file_uploader("Choose a file")
     if uploaded_file is not None:        
+        with st.spinner('Por favor esperar el procesamiento...'):               
 
-        # Can be used wherever a "file-like" object is accepted:
-        st.session_state['dataframe_original'] = pd.read_csv(uploaded_file,sep =';')
+            st.session_state['dataframe_original'] = pd.read_csv(uploaded_file,sep =';')        
+            procesa_cotizacion.proceso(str(st.session_state['dataframe_original']['FECHA'].iloc[-1]))       
+            procesar_datos.proceso()
+            st.success('Listo')
+            col1, col2, col3 = st.columns(3)
+            col1.metric(label="Total datos", value=len(st.session_state['dataframe_original']))
+            #col1.metric("Temperature", "70 °F", "1.2 °F")
+            col2.metric("Wind", "9 mph", "-8%")
+            col3.metric("Humidity", "86%", "4%")
 
-        st.dataframe(st.session_state['dataframe_original'].head())
-        #st.write(st.session_state['dataframe_original']['FECHA'].iloc[-1])
-        procesa_cotizacion.proceso(str(st.session_state['dataframe_original']['FECHA'].iloc[-1]))
-        st.write(st.session_state['datos_ccl'])
-        st.write(st.session_state['datos_ccl_mensual'])
-        procesar_datos.proceso()
-        
 
-
-    col1, col2 = st.columns(2)
-
-    with col1:
-        st.session_state['nombre_dataset'] = st.selectbox(
-            'Seleccionar dataset',
-            ('Iris', 'Insurance'))
-
-        st.write('Dataset seleccionado :', st.session_state['nombre_dataset'])
-
-    with col2:
-        st.session_state['nombre_variable_objetivo'] = str(st.selectbox(
-            'Seleccionar variable objetivo',
-            ('Species', 'smoker')))
-
-        st.write('Objetivo seleccionado:',
-                 st.session_state['nombre_variable_objetivo'])
+    
 
 
 if __name__ == "__main__":
