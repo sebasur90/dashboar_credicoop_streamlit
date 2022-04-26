b498c496b5aa9c1892358c46b41e9d6ef161caa2
Sebastian Rodriguez <seba_sur90@hotmail.com>
2022-04-26 11:41:33 -0300

    Elimino lo innecesario de funciones para datasets

--- funciones_para_datasets/procesar_datos.py
+++ funciones_para_datasets/procesar_datos.py
@@ -1,39 +1,20 @@
-import funciones_para_datasets.procesa_cotizacion
 import pandas as pd
-import datetime as dt
-from datetime import date
 import numpy as np
-import pathlib
+
 import streamlit as st
 
 
 def proceso():
 
-    PATH = pathlib.Path(__file__).parent
-
-    DATA_PATH = PATH.joinpath("../datasets").resolve()
-
     datos_sin_procesar = st.session_state['dataframe_original']
-    #datos_sin_procesar = pd.read_csv(        (DATA_PATH.joinpath("./mov.csv").resolve()), sep=';')
-
     st.session_state['meses_del_ano'] = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio",
                                          "agosto", "septiembre", "octubre", "noviembre", "diciembre"]
-
     st.session_state['meses_del_ano_numeros']=[1,2,3,4,5,6,7,8,9,10,11,12]
-
     st.session_state['anos']=[2018,2019,2020,2021,2022]
-
     datos = datos_sin_procesar.copy()
-
-    # paso a minuscula las columnas y la info la descripcion
     datos.columns = datos.columns.str.lower()
     datos['descripcion'] = datos['descripcion'].str.lower()
-
-    # le doy formato a la fecha
     datos['fecha'] = pd.to_datetime(datos['fecha'], format='%Y%m%d')
-
-    # saca comas y puntos y convierte a numeros las columnas
-
     columnas = ['debito', 'credito', 'saldo']
     for columna in columnas:
         datos[columna] = datos[columna].str.replace('.', '', regex=True)
@@ -42,21 +23,7 @@ def proceso():
 
     anos = range(datos.fecha.iloc[-1].year, datos.fecha.iloc[0].year)
 
-    # procesa_cotizacion.proceso(datos.fecha.iloc[-1])
-
-    ''' dol = pd.read_excel((DATA_PATH.joinpath("./dolar.xlsx")
-                         ).resolve(), sheet_name="cotizacion") '''
-
-    #st.session_state['datos_ccl'] = datos_ccl
-    #st.session_state['datos_ccl_mensual'] = datos_ccl_mensual
-
-    ''' datos_ccl = pd.read_excel(
-        (DATA_PATH.joinpath("./dolar.xlsx")).resolve(), sheet_name="datos_ccl")
-    datos_ccl_mensual = pd.read_excel(
-        (DATA_PATH.joinpath("./dolar.xlsx")).resolve(), sheet_name="datos_ccl_mensual") '''
-
-    # datasets_adicionales.descarga_datasets(datos.fecha.iloc[-1])
-
+    
     conceptos = []
 
     datos['concepto'] = np.where(datos.descripcion.str.contains("pago de servicios ente: "),
@@ -65,6 +32,13 @@ def proceso():
                                  np.where(datos.descripcion.str.contains("compra con tarjeta cabal debito"),
                                           datos.descripcion.map(
                                               lambda x: x[len("compra con tarjeta cabal debito tarj:xxxx comercio:"):]),
+                                          np.where(datos.descripcion.str.contains("transf."),"transferencias", 
+                                            np.where(datos.descripcion.str.contains(
+                                                                    "debito/cred aut-segurcoop seg"),
+                                                                    "debito/cred aut-segurcoop seg",
+                                                                    np.where(datos.descripcion.str.contains("constitucion de plazo fijo"),"constitucion de plazo fijo", 
+                                                                             np.where(datos.descripcion.str.contains("mercado libre") | datos.descripcion.str.contains("mercadolibre"),"mercadolibre", 
+                                                                                      
                                           np.where(
                                               datos.descripcion.str.contains(
                                                   "debito/credito automatico-tarjeta cabal"),
@@ -77,7 +51,7 @@ def proceso():
                                                                             datos.descripcion.str.contains(
                                                                                 "compra/venta de moneda extranjera"),
                                                                             "compra/venta de moneda extranjera", datos.descripcion
-                                                                        ))))))
+                                                                        ))))))))))
 
     ''' dolares = dol.copy()
 
@@ -128,12 +102,17 @@ def proceso():
     #datos_final = datos_final.set_index('fecha')
 
     datos_final['ano'] = pd.DatetimeIndex(datos_final['fecha']).year
+    
+    st.session_state['anos']=datos_final['ano'].sort_values(ascending=True).unique()
+    st.session_state['ultimo_ano']=st.session_state['anos'][-1]
+    
     datos_final['mes'] = pd.DatetimeIndex(datos_final['fecha']).month
     datos_final = datos_final.set_index('fecha')
     datos_final = datos_final.sort_values(by='fecha')    
     datos_final['val_abs']=abs(datos_final['credito'] + datos_final['debito'] )
     st.session_state['datos_procesados']=datos_final
-    st.dataframe(st.session_state['datos_procesados'])
+    st.session_state['datos_procesados'].to_csv("datos_proc.csv")
+    
     
     st.session_state['sueldos']=st.session_state['datos_procesados'][st.session_state['datos_procesados'].concepto =='acreditacion de sueldos ' ]
     st.session_state['sueldos_agrupados_mes_ano']=st.session_state['sueldos'].groupby(['ano','mes'])['val_abs','val_abs_usd_ccl'].sum().reset_index()
@@ -141,28 +120,71 @@ def proceso():
     datos_final.to_csv("datos_2.csv")
     st.session_state['sueldos'].to_csv("sueldos.csv")
     st.session_state['sueldos_agrupados_mes_ano'].to_csv("sueldos_agrupados_mes_ano.csv")
-    st.session_state['sueldos_agrupados_mes_ano']['media_12']=st.session_state['sueldos_agrupados_mes_ano'].val_abs_usd_ccl.rolling(12).mean()
-    st.session_state['sueldos_agrupados_mes_ano']['media_6']=st.session_state['sueldos_agrupados_mes_ano'].val_abs_usd_ccl.rolling(12).mean()
-    st.session_state['sueldos_agrupados_mes_ano']['media_3']=st.session_state['sueldos_agrupados_mes_ano'].val_abs_usd_ccl.rolling(12).mean()
+    st.session_state['sueldos_agrupados_mes_ano']['media_12_usd']=st.session_state['sueldos_agrupados_mes_ano'].val_abs_usd_ccl.rolling(12).mean()
+    st.session_state['sueldos_agrupados_mes_ano']['media_6_usd']=st.session_state['sueldos_agrupados_mes_ano'].val_abs_usd_ccl.rolling(12).mean()
+    st.session_state['sueldos_agrupados_mes_ano']['media_3_usd']=st.session_state['sueldos_agrupados_mes_ano'].val_abs_usd_ccl.rolling(12).mean()
     st.session_state['sueldos_agrupados_mes_ano']['media_12']=st.session_state['sueldos_agrupados_mes_ano'].val_abs.rolling(12).mean()
     st.session_state['sueldos_agrupados_mes_ano']['media_6']=st.session_state['sueldos_agrupados_mes_ano'].val_abs.rolling(12).mean()
     st.session_state['sueldos_agrupados_mes_ano']['media_3']=st.session_state['sueldos_agrupados_mes_ano'].val_abs.rolling(12).mean()
     
-    
+    st.session_state['sueldos_agrupados_ano']=st.session_state['sueldos_agrupados_mes_ano'].groupby('ano')['val_abs_usd_ccl','val_abs'].sum().reset_index()
+
     
     st.session_state['mejor_ano']=st.session_state['sueldos_agrupados_mes_ano'].groupby('ano')['val_abs_usd_ccl','val_abs'].sum()
     
     st.session_state['mejor_ano']=st.session_state['mejor_ano'].reset_index()
     st.session_state['mejor_ano'].columns=['ano','val_abs_usd_ccl','val_abs']
     
+    if len(st.session_state['sueldos_agrupados_mes_ano'][st.session_state['sueldos_agrupados_mes_ano'].ano == st.session_state['sueldos_agrupados_mes_ano'].ano.sort_values().iloc[0]]) <12:
+        st.session_state['mejor_ano']=st.session_state['mejor_ano'][st.session_state['mejor_ano'].ano >st.session_state['sueldos_agrupados_mes_ano'].ano.sort_values().iloc[0]]
+    #st.session_state['mejor_ano'][st.session_state['mejor_ano'].ano > st.session_state['mejor_ano'].ano.sort_values().iloc[0]]
+     
+    
     st.session_state['mejor_mes']=st.session_state['sueldos_agrupados_mes_ano'].groupby('mes')['val_abs_usd_ccl','val_abs'].sum()
     st.session_state['mejor_mes']=st.session_state['mejor_mes'].reset_index()
-    st.session_state['mejor_mes'].columns=['mes','val_abs_usd_ccl','val_abs']
-    
-    
+    st.session_state['mejor_mes'].columns=['mes','val_abs_usd_ccl','val_abs'] 
     st.session_state['pivot'] = st.session_state['sueldos'].groupby(['ano', 'mes'], as_index=False)[
                 'val_abs_usd_ccl'].sum()
     st.session_state['pivot']['val_abs_usd_ccl'] = st.session_state['pivot']['val_abs_usd_ccl'].round()
     st.session_state['pivot'] = st.session_state['pivot'].pivot(index='mes', columns='ano',
                         values='val_abs_usd_ccl')
     
+   
+    st.session_state['gastos'] = st.session_state['datos_procesados'][(st.session_state['datos_procesados']['debito'] > 0) &
+                              (st.session_state['datos_procesados']['concepto'] != "suscripcion a fondo comun de inversion ") &
+                              (st.session_state['datos_procesados']['concepto'] != "constitucion de plazo fijo") &
+                              (st.session_state['datos_procesados']['concepto'] != "compra/venta de moneda extranjera")&
+                              (st.session_state['datos_procesados']['concepto'] != "transferencias")]
+    
+    
+    
+    st.session_state['gastos_tarjetas'] = st.session_state['gastos'][(st.session_state['gastos']['debito'] > 0) &
+                                        (st.session_state['gastos']['concepto'] == "cabal") |
+                                        (st.session_state['gastos']['concepto'] == "visa")]
+    
+    st.session_state['gastos_agrupados_mes_ano']=st.session_state['gastos'].groupby(['ano','mes'])['val_abs','val_abs_usd_ccl'].sum().reset_index()
+    st.session_state['gastos_agrupados_ano']=st.session_state['gastos'].groupby('ano')['val_abs_usd_ccl','val_abs'].sum().reset_index()
+    
+    st.session_state['gastos_agrupados_mes_ano']['media_12_usd']=st.session_state['gastos_agrupados_mes_ano'].val_abs_usd_ccl.rolling(12).mean()
+    st.session_state['gastos_agrupados_mes_ano']['media_6_usd']=st.session_state['gastos_agrupados_mes_ano'].val_abs_usd_ccl.rolling(12).mean()
+    st.session_state['gastos_agrupados_mes_ano']['media_3_usd']=st.session_state['gastos_agrupados_mes_ano'].val_abs_usd_ccl.rolling(12).mean()
+    st.session_state['gastos_agrupados_mes_ano']['media_12']=st.session_state['gastos_agrupados_mes_ano'].val_abs.rolling(12).mean()
+    st.session_state['gastos_agrupados_mes_ano']['media_6']=st.session_state['gastos_agrupados_mes_ano'].val_abs.rolling(12).mean()
+    st.session_state['gastos_agrupados_mes_ano']['media_3']=st.session_state['gastos_agrupados_mes_ano'].val_abs.rolling(12).mean()
+    
+    st.session_state['gastos_agrupados_mes_ano_tarjetas']=st.session_state['gastos_tarjetas'].groupby(['ano','mes'])['val_abs','val_abs_usd_ccl'].sum().reset_index()
+    st.session_state['gasto_agrupados_ano_tarjetas']=st.session_state['gastos_tarjetas'].groupby('ano')['val_abs_usd_ccl','val_abs'].sum().reset_index()
+    
+    st.session_state['peor_ano']=st.session_state['gastos_agrupados_mes_ano'].groupby('ano')['val_abs_usd_ccl','val_abs'].sum()
+    
+    st.session_state['peor_ano']=st.session_state['peor_ano'].reset_index()
+    st.session_state['peor_ano'].columns=['ano','val_abs_usd_ccl','val_abs']
+    
+    if len(st.session_state['gastos_agrupados_mes_ano'][st.session_state['gastos_agrupados_mes_ano'].ano == st.session_state['gastos_agrupados_mes_ano'].ano.sort_values().iloc[0]]) <12:
+        st.session_state['peor_ano']=st.session_state['peor_ano'][st.session_state['peor_ano'].ano >st.session_state['gastos_agrupados_mes_ano'].ano.sort_values().iloc[0]]
+    
+    
+    st.session_state['peor_mes']=st.session_state['gastos_agrupados_mes_ano'].groupby('mes')['val_abs_usd_ccl','val_abs'].sum()
+    st.session_state['peor_mes']=st.session_state['peor_mes'].reset_index()
+    st.session_state['peor_mes'].columns=['mes','val_abs_usd_ccl','val_abs']     
+    st.session_state['gastos_tarjetas'].to_csv("gastos_tarjetas.csv")
